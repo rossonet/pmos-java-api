90fd04aa370816b5c5a1a4cccee231f43b2cf403
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2023-01-31 02:40:01 +0100

    creata client class

---
+++ src/main/java/net/rossonet/pmos/client3/ProcessMakerClient3.java
@@ -0,0 +1,12 @@
+package net.rossonet.pmos.client3;
+
+/**
+ * documention: https://wiki.processmaker.com/3.3/ProcessMaker_WSDL_Web_Services
+ * demo server: https://processi.bottegaio.net/sys/en/neoclassic/
+ *
+ * @author Andrea Ambrosini [Rossonet s.c.a r.l.]
+ *
+ */
+public class ProcessMakerClient3 {
+
+}
